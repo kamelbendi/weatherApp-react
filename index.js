fc7b7c96256d9755ae358d2f3c039aa00e023ad2
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-07-30 20:18:40 +0200

    adding styling and few more functionnalities

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function App() {
     const [lat, setLat] = React.useState([]);
     const [long, setLong] = React.useState([]);
     const [data, setData] = React.useState([]);
-    /* const [markUp, setMarkUp] = React.useState('') */
+    
+
+    //useEffect to fetch the data from the API
 
     React.useEffect(() => {
 
@@ -26,79 +28,50 @@ function App() {
           });
         }
         fetchData();
+
+
       }, [lat,long])
 
-    // main function that retrieves data
-
-    /* function weather() {
-
-        var apiKey = 'dcd17df9ff1af5e8853086f7f8b82a0b';
-        var url = 'https://api.openweathermap.org/data/2.5/weather?lat='+{lat}+'&lon='+{long}+'&appid='+apiKey;
-    
-        navigator.geolocation.getCurrentPosition(success, error);
-    
-        
-        function success(position) {
-
-          setLat(position.coords.latitude);
-          setLong(position.coords.longitude);
-          
-          /* location.innerHTML = 'Latitude is ' + latitude + '° Longitude is ' + longitude + '°'; */
-          /*
-          setMarkUp('Latitude is {lat} and longitude is {long}');
-          
-           $.getJSON(url, function(data) {
-            $('#temp').html(data.currently.temperature + '° F');
-            $('#minutely').html(data.minutely.summary);
-          });
-        }
-    
-        function error() {
-          /* location.innerHTML = "Unable to retrieve your location"; */
-          /*
-          setMarkUp('Unable to retrieve your location');
-        }
-    
-        /* location.innerHTML = "Locating..."; */
-        /*
-        setMarkUp('Locating...');
-        
-      }
- */
-    
-
+   
     return (
-        <div>
-            <h1>FreeCodeCamp Weather App</h1>
+        <div className="main">
             
-            {/* <div id="location" dangerouslySetInnerHTML={{__html: markUp}}/>
-            <div id="temp"></div>
-            <div id="minutely"></div> */}
-
-        {(typeof data.main != 'undefined') ? (
-            <Weather weatherData={data}/>
-        ): (
-            <div>No data to be shown</div>
-            )}
-            
-
+            {(typeof data.main != 'undefined') ? (
+                <Weather weatherData={data}/>
+            ): (
+                <div>
+                    cannot access your location!
+                    {console.log(data)}
+                </div>
+                )}
         </div>
     );
 
 }
 
+const refresh = () => {
+    window.location.reload();
+  }
+
 function Weather({weatherData}){
     return (
         <>
-            <h3>{weatherData.name}</h3>
-
-            <p>wind speed : {weatherData.wind.speed}</p>
-            <p>Temprature: {Math.round(weatherData.main.temp - 273.15)} &deg;C</p>
-            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Description: {weatherData.weather[0].main}</p>
-            <p>Humidity: {weatherData.main.humidity} %</p>
-            
+            <div className="top">
+                <div className="header">{weatherData.name}</div>
+                <button onClick={refresh}>Refresh Page</button>
+            </div>
+            <div className="flex">
+                <p className="temp">wind speed : {weatherData.wind.speed} Klm/h</p>
+                <p className="temp">Temprature: {Math.round(weatherData.main.temp - 273.15)} &deg;C</p>
+            </div>
+            <div className="flex">
+                <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+                <p className="description">Description: {weatherData.weather[0].main}</p>
+                </div>
+            <div className="flex">
+                <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+                <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            </div>
         </>
     );
 }
